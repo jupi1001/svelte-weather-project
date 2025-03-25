da4ad376b99d7af73e2ed2e71d82edae895d2f9e
Laurids <realjupi1001@gmail.com>
2025-03-25 15:33:54 +0100

    Update SvelteKit configuration to use Vercel adapter, add weather route with prerendering disabled, and include new adapter dependency in package.json and package-lock.json.

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,12 @@
-import adapter from '@sveltejs/adapter-auto';
+import vercel from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter(),
+    adapter: vercel(), 
     prerender: {
-      entries: ['/weather']
+      entries: ['*'] 
     }
   },
   preprocess: vitePreprocess()
